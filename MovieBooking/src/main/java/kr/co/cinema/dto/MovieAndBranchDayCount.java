cade0c221eaccf4242a4a07ed56a48b7566ba0a9
SMART20103\Administrator <jby9215@gmail.com>
2017-02-27 09:24:15 +0900

    서블릿 컨텍스트 수정

---
+++ MovieBooking/src/main/java/kr/co/cinema/dto/MovieAndBranchDayCount.java
@@ -0,0 +1,33 @@
+package kr.co.cinema.dto;
+
+//메인화면 영화 매출표 dto
+public class MovieAndBranchDayCount {
+
+	private String movKorName;
+	private String movOpenDate;
+	private int brcCntClientCount;
+	public String getMovKorName() {
+		return movKorName;
+	}
+	public void setMovKorName(String movKorName) {
+		this.movKorName = movKorName;
+	}
+	public String getMovOpenDate() {
+		return movOpenDate;
+	}
+	public void setMovOpenDate(String movOpenDate) {
+		this.movOpenDate = movOpenDate;
+	}
+	public int getBrcCntClientCount() {
+		return brcCntClientCount;
+	}
+	public void setBrcCntClientCount(int brcCntClientCount) {
+		this.brcCntClientCount = brcCntClientCount;
+	}
+	@Override
+	public String toString() {
+		return "MovieAndBranchDayCount [movKorName=" + movKorName + ", movOpenDate=" + movOpenDate
+				+ ", brcCntClientCount=" + brcCntClientCount + "]";
+	}
+
+}
