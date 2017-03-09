6f3fc751371995ab5e7521f817979addd62acb2e
김진남 <better4307@gmail.com>
2017-03-09 13:48:17 +0900

    analysis 수정중

--- MovieBooking/src/main/java/kr/co/cinema/admin/AdminController.java
+++ MovieBooking/src/main/java/kr/co/cinema/admin/AdminController.java
@@ -337,28 +337,5 @@ public class AdminController {
 		logger.debug(" Controller updateCost get실행");
 		return "management/costModify";
 	}
-	/************************************************************************************************************
-	통계 관리 메서드
-	************************************************************************************************************/	
-	
-	//***영화별 예매/매출조회 : 페이지이동
-	@RequestMapping(value="analisysByMovie", method=RequestMethod.GET)
-	public String selectAnalisysByMovie() {
-		logger.debug(" Controller selectAnalisysByMovie get실행");
-		return "analisys/analisysByMovie";
-	}
-	
-	//***지점별 예매/매출 : 페이지이동
-	@RequestMapping(value="analisysByBranch", method=RequestMethod.GET)
-	public String selectAnalisysByBranch() {
-		logger.debug(" Controller selectAnalisysByMovie get실행");
-		return "analisys/analisysByBranch";
-	}
-	
-	//***날짜별 예매,매출 : 페이지이동
-	@RequestMapping(value="analisysByDate", method=RequestMethod.GET)
-	public String selectAnalisysByDate() {
-		logger.debug(" Controller selectAnalisysByDate get실행");
-		return "analisys/analisysByDate";
-	}
+
 }
